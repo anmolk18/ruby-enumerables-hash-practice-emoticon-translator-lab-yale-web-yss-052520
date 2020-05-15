7ac96d793150d5337eec0937ee8d7750fe2a4a37
Anmolpreet Kaur <anmolpreet.kaur@yale.edu>
2020-05-15 22:22:25 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,11 +1,33 @@
-# require modules here
+require "yaml"
+require 'pry'
 
-def load_library
-  # code goes here
+
+def load_library(file_path)
+  emoticons = YAML.load_file(file_path)
+  new_hash = {}
+  emoticons.each do |key,value|
+    new_hash[key] = {}
+    new_hash[key][:english] = value[0]
+    new_hash[key][:japanese] = value[1]
+  end
+  new_hash
 end
 
-def get_japanese_emoticon
-  # code goes here
+
+    #emoticons.find {|key,value| value[:english]== emoticon}
+def get_japanese_emoticon(file_path, emoticon)
+  emoticons = load_library(file_path)
+  answer = nil
+  emoticons.each do |key, value| 
+    if value[:english] == emoticon
+      answer = value[:japanese]
+    end 
+  end 
+  if answer == nil 
+    "Sorry, that emoticon was not found"
+  else 
+    answer
+  end 
 end
 
 def get_english_meaning
