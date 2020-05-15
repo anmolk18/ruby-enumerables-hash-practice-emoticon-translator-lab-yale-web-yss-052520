c435812118e855c41a96d6940144e3ef627c9137
Anmolpreet Kaur <anmolpreet.kaur@yale.edu>
2020-05-15 22:24:23 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -30,7 +30,7 @@ def get_japanese_emoticon(file_path, emoticon)
 end
 
 
-def get_english_meaning
+def get_english_meaning(file_path, emoticon)
   emoticons = load_library(file_path)
   answer = nil
   emoticons.each do |key, value| 
